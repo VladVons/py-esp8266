195ab70c1d143866ac07717c607798336889e250
VladVons <vladvons@gmail.com>
2017-04-04 07:31:01 +0300

    just commit

--- airplan/PC/SockClientUDP.py
+++ airplan/PC/SockClientUDP.py
@@ -25,61 +25,18 @@ ArrMotor2 = [14, 16]
 
 #-----------
 
-class TSockClientUDP():
-    def __init__(self, aHost, aPort):
-        self.Host  = aHost
-        self.Port  = aPort
-
-        self.Start    = time.time()
-        self.SendCnt  = 0
-        self.TimeOut  = 0
-        self.MaxTries = 3
+class TSerial():
+    def __init__(self):
         self.Clear()
-
-    def __del__(self):
-        TotalSec = round(time.time() - self.Start, 2)
-        if (self.SendCnt > 0):
-            print("TotalSec", TotalSec, 
-              "Avg", round(TotalSec /  self.SendCnt, 3),
-              "SendCnt", self.SendCnt, 
-              "TimeOut", self.TimeOut)
 
     def Clear(self):
         self.Data = []
 
+    def GetData(self):
+        return self.Data
+
     def Add(self, aData):
         self.Data.append(aData)
-
-    def Send(self, aTimeOut = 0.2):
-        self.SendCnt += 1
-        self.Print("--- " +  str(self.SendCnt))
-
-        DataOut = json.dumps(self.Data) 
-        self.Clear()
-        print("DataOut Len", len(DataOut),  DataOut)
-
-        Tries  = self.MaxTries
-        DataIn = None
-        while (DataIn == None and Tries > 0):
-            Tries  -= 1
-
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.settimeout(aTimeOut)
-            sock.sendto(DataOut, (self.Host, self.Port))
-            try:
-                DataIn = sock.recvfrom(1024)
-                print("")
-                print("DataIn len", len(DataIn[0]), DataIn)
-            except:
-                self.TimeOut += 1
-                print('Timeout', self.TimeOut, "Tries", Tries)
-
-        if (DataIn):
-            Result = json.loads(DataIn[0])
-        else:
-            Result = {}
-        return Result
-
 
     def Show(self):
         print(self.Data)
@@ -104,8 +61,8 @@ class TSockClientUDP():
     def SetPinInv(self, aPin):
         self.AddFunc("SetPinInv", [aPin])
 
-    def GetPwm(self, aPin):
-        self.AddFunc("GetPwm", [aPin])
+    def GetPwmDuty(self, aPin):
+        self.AddFunc("GetPwmDuty", [aPin])
 
     def SetPwm(self, aPin, aFreq, aDuty):
         self.AddFunc("SetPwmFreq", [aPin, aFreq])
@@ -137,59 +94,114 @@ class TSockClientUDP():
     def SetPwmOffArr(self, aPins):
         self.AddFunc("SetPwmOffArr", [aPins])
 
+
+
+class TSockClientUDP():
+    def __init__(self, aHost, aPort):
+        self.Host  = aHost
+        self.Port  = aPort
+
+        self.Start    = time.time()
+        self.SendCnt  = 0
+        self.TimeOut  = 0
+        self.MaxTries = 3
+
+    def __del__(self):
+        TotalSec = round(time.time() - self.Start, 2)
+        if (self.SendCnt > 0):
+            print("TotalSec", TotalSec, 
+              "Avg", round(TotalSec /  self.SendCnt, 3),
+              "SendCnt", self.SendCnt, 
+              "TimeOut", self.TimeOut)
+
+    def Send(self, aData, aTimeOut = 0.2):
+        self.SendCnt += 1
+
+        DataOut = json.dumps(aData) 
+        print("DataOut Len", len(DataOut),  DataOut)
+
+        Tries  = self.MaxTries
+        DataIn = None
+        while (DataIn == None and Tries > 0):
+            Tries  -= 1
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.settimeout(aTimeOut)
+            sock.sendto(DataOut, (self.Host, self.Port))
+            try:
+                DataIn = sock.recvfrom(1024)
+                print("")
+                print("DataIn len", len(DataIn[0]), DataIn)
+            except:
+                self.TimeOut += 1
+                print('Timeout', self.TimeOut, "Tries", Tries)
+
+        if (DataIn):
+            Result = json.loads(DataIn[0])
+        else:
+            Result = {}
+        return Result
+
+
 #-----------
 
 class TEsp():
     def __init__(self, aHost, aPort):
-        self.SC = TSockClientUDP(aHost, aPort)
+        self.Sock   = TSockClientUDP(aHost, aPort)
+        self.Serial = TSerial()
+
+    def Send(self, aTimeOut = 0.2):
+        self.Serial.Print("---")
+        self.Sock.Send(self.Serial.GetData(), aTimeOut)
+        self.Serial.Clear()
 
     def GetInfo(self):
-        self.SC.Clear()
-        self.SC.AddFunc("GetInfo")
-        self.SC.AddFunc("GetTicks")
-        self.SC.AddFunc("GetMemFree")
-        #self.SC.AddFunc("GetMachineId")
+        self.Serial.AddFunc("GetInfo")
+        self.Serial.AddFunc("GetTicks")
+        self.Serial.AddFunc("GetMemFree")
+        #self.Serial.AddFunc("GetMachineId")
+        #self.Serial.Show()
+        self.Send()
 
-        #self.SC.Show()
-        self.SC.Send()
+    def GetPinImfo(self, aPins) :
+        for Pin in aPins:
+            self.Serial.GetPin(Pin)
+            self.Serial.GetPwmDuty(Pin)
+            self.Send()
 
     def LedFlash(self, aCnt):
-        self.SC.Clear()
         for i in range(aCnt):
-            self.SC.SetPinInvArr([cLed_Red, cLed_Green, cLed_Blue])
-            #self.SC.AddFunc("Sleep", [50])
-            self.SC.Send(0.2)
+            #self.Serial.SetPinInvArr([cLed_Red, cLed_Green, cLed_Blue])
+            #self.Serial.AddFunc("Sleep", [50])
+            self.Send(0.2)
 
     def MotorStop(self, aPins):
-        self.SC.Clear()
-        self.SC.SetPwmOffArr(aPins)
-        self.SC.SetPinArr(aPins, 1)
-        self.SC.Send()
+        self.Serial.SetPwmOffArr(aPins)
+        self.Serial.SetPinArr(aPins, 1)
+        self.Send()
 
     def Motor(self, aPins, aSpeed):
-        self.SC.Clear()
-
         Forward = aSpeed > 0
         PinA = aPins[int(Forward)]
         PinB = aPins[int(not Forward)]
 
         if (aSpeed == 0):
              self.MotorStop(Pins)
-             self.SC.Send()
+             self.Send()
         else:
             Speed = abs(aSpeed)
             if (Speed > 1000):
                 Speed = 1000
 
-            self.SC.SetPwmOff(PinB)
-            self.SC.SetPwm(PinA, 100, Speed)
-            self.SC.SetPin(PinB, 0)
-            self.SC.SetPin(PinA, 1)
-            self.SC.Send()
+            self.Serial.SetPwmOff(PinB)
+            self.Serial.SetPwm(PinA, 100, Speed)
+            self.Serial.SetPin(PinB, 0)
+            self.Serial.SetPin(PinA, 1)
+            self.Send()
 
     def Exec(self, aScript, aTimeOut = 0.2):
-        self.SC.Exec(aScript)
-        self.SC.Send(aTimeOut)
+        self.Serial.Exec(aScript)
+        self.Send(aTimeOut)
 
 #-----------
 
@@ -203,7 +215,6 @@ def TestLamp(aCnt):
     Esp.MotorStop(ArrMotor1)
     Esp.LedFlash(aCnt)
 
-
 def TestExec():
     Esp = TEsp("192.168.2.119", 51015)
     Esp.Exec("SetPinInv(15);Sleep(200);SetPinInv(15);Sleep(200);SetPinInv(15)", 3)
@@ -211,14 +222,19 @@ def TestExec():
 
 def TestCall():
     Esp = TEsp("192.168.2.119", 51015)
-    Esp.SC.Clear()
-    Esp.SC.Add({"Func": "GetInfo", "Args": []})
-    Esp.SC.Send()
+    Esp.Sock.Add({"Func": "GetInfo", "Args": []})
+    Esp.Sock.Send()
 
-TestLamp(3)
+def TestPinInfo():
+    Esp = TEsp("192.168.2.119", 51015)
+    Esp.GetPinImfo(ArrLed)
+
+
+#TestLamp(3)
 #TestMotor(-200)
 #TestExec()
 #TestCall()
+TestPinInfo()
 
 #Class = globals()["Test"]
 #Class.Print("Hello")
