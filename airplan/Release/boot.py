783e35415b6993bb7130aa0b971365f531aa01cf
VladVons <vladvons@gmail.com>
2017-04-08 00:15:35 +0300

    just commit

---
+++ airplan/Release/boot.py
@@ -0,0 +1,23 @@
+#---VladVons@gmail.com
+# 04.02.17
+# micropython ESP8266
+#---
+
+
+# This file is executed on every boot (including wake-boot from deepsleep)
+
+#import esp
+#esp.osdebug(None)
+import gc
+#import webrepl
+#webrepl.start()
+
+#import machine
+#import gc
+#import time
+#import os
+#import sys
+
+gc.collect()
+print("MemFree", gc.mem_free(), "MemAlloc", gc.mem_alloc())
+print("Hello from ESP8266 MicroPython")
