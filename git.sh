f92c41a5c87cdbb4431bb7c04b4837032fbe1aac
linux <vladvons@gmail.com>
2017-11-06 07:55:59 +0200

    MyCommit

--- git.sh
+++ git.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-
 # Created: 28.09.2016
 # Vladimir Vons, VladVons@gmail.com
 
+User="VladVons"
+Mail="vladvons@gmail.com"
+Url="https://github.com/$User/py-esp8266.git"
+
+
+Log()
+{
+  aMsg="$1";
+
+  Msg="$(date +%Y-%m-%d-%a), $(date +%H:%M:%S), $(id -u -n), $aMsg"
+  echo "$Msg"
+}
 
 Clean()
 {
   echo "delete objects"
   find . -name '*.pyc' -exec rm -v -f -R {} \;
   find . -name '*.log' -exec rm -v -f -R {} \;
+  find . -name '__pycache__' -exec rm -v -f -R {} \;
 
   echo
   echo "Statistics *.py"
@@ -22,28 +34,37 @@ Clean()
 
 GitAuth()
 {
+  Log "$0->$FUNCNAME"
+
+  sudo chown -R $USER .
+
   # sign with eMail
-  git config --global user.email "vladvons@gmail.com"
+  git config --global user.email $Mail
+  git config --global user.name $User
 
   # no password 
-  git config --global credential.helper 'cache --timeout=360000'
+  git config --global credential.helper 'cache --timeout=36000'
 }
 
 GitCreate()
 {
+  Log "$0->$FUNCNAME"
+
   # create new project on disk
   git init
   GitAuth
 
   # remote git server location
-  git remote add origin https://github.com/VladVons/py-esp8266.git
+  git remote add origin $Url
 
 }
 
 GitClone()
 {
+  Log "$0->$FUNCNAME"
+
   # restore clone copy fromserver to disk 
-  git clone https://github.com/VladVons/py-esp8266.git
+  git clone $Url
   GitAuth
 
   #web admin access here
@@ -54,6 +75,9 @@ GitClone()
 GitSyncToServ()
 # sync only changes from disk to server 
 {
+  aComment="$1";
+  Log "$0->$FUNCNAME"
+
   git status
 
   #git add install.sh
@@ -62,31 +86,40 @@ GitSyncToServ()
   #git log
 
   git add -u -v
-  git commit -a -m "libs"
+  git commit -a -m "$aComment"
   git push -u origin master
 }
 
 GitFromServ()
 # sync changes from server to disk
 {
+  Log "$0->$FUNCNAME"
+
   git pull
 }
 
 GitToServ()
 # sync changes from disk to serv
 {
+  aComment=${1:-"MyCommit"};
+  Log "$0->$FUNCNAME"
+
   Clean
   # add all new files
   git add -A -v
-  GitSyncToServ
+  GitSyncToServ "$aComment"
 }
 
+Diff(){
+  diff -r dir1 dir2 | sed '/Binary\ files\ /d' > diff.txt
+}
 
 #GitUpdate
 
 clear
 case $1 in
     Clean)              "$1"        "$2" "$3" ;;
+    GitAuth)            "$1"        "$2" "$3" ;;
     GitCreate)          "$1"        "$2" "$3" ;;
     GitToServ|t)        GitToServ   "$2" "$3" ;;
     GitFromServ|f)      GitFromServ "$2" "$3" ;;
